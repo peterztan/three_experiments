182b8f5a302431b395d839f0b1014d7bb43514d5
peterztan <pt072093@gmail.com>
2022-01-25 06:13:33 -0600

    set up three basic scene structure

--- src/world-composer.ts
+++ src/world-composer.ts
@@ -6,6 +6,9 @@
 
 import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {GLTF, GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
 
 /**
  * An example element.
@@ -25,39 +28,64 @@ export class WorldComposer extends LitElement {
     }
   `;
 
-  /**
-   * The name to say "Hello" to.
-   */
-  @property()
-  name = 'World';
+  @property({type: String, reflect: true, attribute: 'src-set'}) srcSet = '';
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+  private loader: GLTFLoader;
+  private dracoLoader: DRACOLoader;
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private renderer: THREE.WebGLRenderer;
+
+  constructor() {
+    super();
+
+    // Instantiate a loader
+    this.loader = new GLTFLoader();
+
+    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+    this.dracoLoader = new DRACOLoader();
+
+
+    this.scene = new Scene();
+    this.camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    this.renderer = new WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+  }
+
+  private onGLTFloaded(gltf: GLTF) {
+    this.scene.add(gltf.scene);
+  }
+
+  private onProgress(xhr: ProgressEvent) {
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  }
+
+  private onError(error: ErrorEvent) {
+    console.error(error);
+  }
 
   override render() {
-    return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `;
+    this.dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
+    this.loader.setDRACOLoader( this.dracoLoader );
+
+    // Load a glTF resource
+    this.loader.load(
+      // resource URL
+      'models/gltf/duck/duck.gltf',
+      // called when the resource is loaded
+      this.onGLTFloaded,
+      // called while loading is progressing
+      this.onProgress,
+      // called when loading has errors
+      this.onError
+    );
+    return html`${this.renderer.domElement}`;
   }
 
-  private _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
-  }
-
-  /**
-   * Formats a greeting
-   * @param name The name to say "Hello" to
-   */
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
+  override connectedCallback(): void {
+      super.connectedCallback();
+      this.renderer.render(this.scene, this.camera);
   }
 }
 
